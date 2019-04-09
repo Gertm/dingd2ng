8bc574fe0dc3ce486d3628720130efecd01cf51c
Gert Meulyzer <cel357@gmail.com>
2019-04-09 12:43:01 +0200

    fixed the URL catching

--- dingd2ng.nim
+++ dingd2ng.nim
@@ -60,17 +60,18 @@ proc onIrcEvent(client: AsyncIrc, event: IrcEvent) {.async.} =
     await client.reconnect()
   of EvMsg:
     if event.cmd == MPrivMsg:
-      if event.params.len() > 3:
-        var msg = event.params[event.params.high]
-        # if msg == "!test": await client.privmsg(event.origin, "hello")
-        if msg == "!lag":
-          await client.privmsg(event.origin, formatFloat(client.getLag))
-        if unicode.toLower(msg).contains("http"):
-          for part in msg.split(' '):
-            if unicode.toLower(part).startsWith("http"):
-              let title = htmltitle.readTitle(part)
-              if title.len() != 0:
-                await client.privmsg(event.origin, title)
+      var msg = event.params[event.params.len-1]
+      # if msg == "!test": await client.privmsg(event.origin, "hello")
+      if msg == "!lag":
+        await client.privmsg(event.origin, formatFloat(client.getLag))
+      if unicode.toLower(msg).contains("http"):
+        for part in msg.split(' '):
+          if unicode.toLower(part).startsWith("http"):
+            let title = htmltitle.readTitle(part)
+            if title.len() != 0:
+              await client.privmsg(event.origin, title)
+      if unicode.tolower(msg).startsWith("!say"):
+        await client.privmsg(event.origin, "You just said it.")
     echo(event.raw)
 
 var client = newAsyncIrc(
